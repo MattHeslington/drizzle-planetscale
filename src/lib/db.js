5a311988364a31464f9c38a6ba13abb7d9db7ee9
Matt Heslington <matt.heslington@icloud.com>
2023-05-21 19:36:56 +0800

    chore(.gitignore): add .env to gitignore refactor(db.js): remove newline at end of file

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,8 +1,10 @@
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { connect } from '@planetscale/database'
+
 const config = connect ({
     host: import.meta.env.VITE_DATABASE_HOST,
     username: import.meta.env.VITE_DATABASE_USERNAME,
     password: import.meta.env.VITE_DATABASE_PASSWORD
 })
+
 export const db = drizzle(config);
